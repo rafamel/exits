02980adab5c2716a79d68eab9e2c428bf52a01a6
Rafa Mel <melrcm@gmx.net>
2019-01-16 17:33:32 +0100

    Adds method [on]

---
+++ src/methods/on.ts
@@ -0,0 +1,21 @@
+import { store } from '~/store';
+import { TTriggered, IAttach } from '~/types';
+
+export default function on(
+  event: 'triggered',
+  cb: (triggered: TTriggered) => void
+): void;
+export default function on(event: 'done', cb: (done: boolean) => void): void;
+export default function on(
+  event: 'attached',
+  cb: (attached: IAttach) => void
+): void;
+export default function on(
+  event: 'triggered' | 'done' | 'attached',
+  cb: (val: any) => void
+) {
+  if (store.subscribers.hasOwnProperty(String(event))) {
+    // @ts-ignore
+    store.subscribers[event].push(cb);
+  }
+}
