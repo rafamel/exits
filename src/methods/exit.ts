dbc24ca38c018b5db86981bd9c38cadf5c6508d2
Rafa Mel <melrcm@gmx.net>
2019-01-18 10:03:42 +0100

    Always exit with resolver

--- src/methods/exit.ts
+++ src/methods/exit.ts
@@ -4,7 +4,7 @@ import { IStore } from '~/types';
 export default async function exit(store: IStore, code: number): Promise<void> {
   if (store.state.attached && store.state.attached.exit) {
     return handler(store, 'exit', code);
-  } else {
-    process.exit(code);
   }
+
+  return store.options.resolver('exit', code);
 }
