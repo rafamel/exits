16e2aa817afbbeb6c1792da6d4d27e8cd3833fd3
Rafa Mel <melrcm@gmx.net>
2019-01-21 18:35:35 +0100

    on throws on invalid event

--- src/methods/on.ts
+++ src/methods/on.ts
@@ -20,8 +20,9 @@ export default function on(
   event: 'triggered' | 'done' | 'attached',
   cb: (val: any) => void
 ): void {
-  if (store.subscribers.hasOwnProperty(String(event))) {
-    // @ts-ignore
-    store.subscribers[event].push(cb);
+  if (!store.subscribers.hasOwnProperty(String(event))) {
+    throw Error(`Event ${event} doesn't exit on 'exits'`);
   }
+  // @ts-ignore
+  store.subscribers[event].push(cb);
 }
