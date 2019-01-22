87c6ddd0e29d3bfedbe61ac6348ce79a332d920e
Rafa Mel <melrcm@gmx.net>
2019-01-22 10:43:51 +0100

    setState as promise returning function

--- src/utils/set-state.ts
+++ src/utils/set-state.ts
@@ -1,16 +1,18 @@
 import { IState } from '~/types';
 import logger from '~/logger';
 import store from '~/store';
+import { parallel } from 'promist';
 
-export default function setState(update: Partial<IState>): void {
+export default function setState(update: Partial<IState>): Promise<void> {
   const { state, subscribers } = store;
-  Object.entries(update).forEach(([key, value]) => {
+  return parallel.each(Object.entries(update), async ([key, value]) => {
     // @ts-ignore
     state[key] = value;
     if (subscribers.hasOwnProperty(key)) {
       // @ts-ignore
-      subscribers[key].forEach(async (fn) => {
+      await parallel.each(subscribers[key], async (fn) => {
         try {
+          // @ts-ignore
           await fn(value);
         } catch (e) {
           logger.error(e);
