fa51ebf1d604e30e76634d55f8a93907012358bc
Rafa Mel <melrcm@gmx.net>
2019-01-27 23:56:07 +0100

    Adds tests for killWait

--- test/reset-store.ts
+++ test/reset-store.ts
@@ -1,8 +1,35 @@
 import deep from 'lodash.clonedeep';
 import store from '~/store';
 
-export const cloned = deep({ ...store, process: null });
 export default function reset() {
   // tslint:disable-next-line object-literal-shorthand
   Object.assign(store, { ...deep(cloned), process: process });
+}
+
+export const cloned = deep({ ...store, process: null });
+
+export function populateProcesses() {
+  store.processes = {
+    foo: {
+      // @ts-ignore
+      ps: { on: jest.fn(), kill: jest.fn() },
+      opts: {},
+      triggered: false,
+      running: true
+    },
+    bar: {
+      // @ts-ignore
+      ps: { on: jest.fn(), kill: jest.fn() },
+      opts: {},
+      triggered: false,
+      running: true
+    },
+    baz: {
+      // @ts-ignore
+      ps: { on: jest.fn(), kill: jest.fn() },
+      opts: {},
+      triggered: false,
+      running: true
+    }
+  };
 }
---
+++ test/utils/kill-wait.test.ts
@@ -0,0 +1,190 @@
+import store from '~/store';
+import reset, { populateProcesses as populate } from '../reset-store';
+import killWait from '~/utils/kill-wait';
+import { wait } from 'promist';
+
+const implementation = async (ev: string, cb: any) => {
+  if (ev === 'close') {
+    await wait(750);
+    cb();
+  }
+};
+
+test(`Returns immediately when spawned.wait = 'none'`, async () => {
+  expect.assertions(1);
+  reset();
+  populate();
+  store.options.spawned.wait = 'none';
+
+  let res;
+  killWait().then(() => (res = true));
+  await wait(100);
+  expect(res).toBe(true);
+});
+
+test(`Returns immediately when there are no currently running processes`, async () => {
+  expect.assertions(1);
+  reset();
+  populate();
+  store.options.spawned.wait = 'all';
+  store.processes.foo.running = false;
+  store.processes.bar.running = false;
+  store.processes.baz.running = false;
+
+  let res;
+  killWait().then(() => (res = true));
+  await wait(100);
+  expect(res).toBe(true);
+});
+
+test(`Returns immediately when there are no running bind processes for spawned.wait = 'bind'`, async () => {
+  expect.assertions(1);
+  reset();
+  populate();
+  store.options.spawned.wait = 'bind';
+  store.processes.foo.opts.detached = true;
+  store.processes.bar.running = false;
+  store.processes.baz.running = false;
+
+  let res;
+  killWait().then(() => (res = true));
+  await wait(100);
+  expect(res).toBe(true);
+});
+
+test(`Returns immediately when there are no running detached processes for spawned.wait = 'detached'`, async () => {
+  expect.assertions(1);
+  reset();
+  populate();
+  store.options.spawned.wait = 'detached';
+  store.processes.foo.opts.detached = true;
+  store.processes.foo.running = false;
+
+  let res;
+  killWait().then(() => (res = true));
+  await wait(100);
+  expect(res).toBe(true);
+});
+
+test(`Returns immediately when there are no running detached processes for spawned.wait = 'detached'`, async () => {
+  expect.assertions(1);
+  reset();
+  populate();
+  store.options.spawned.wait = 'detached';
+  store.processes.foo.opts.detached = true;
+  store.processes.foo.running = false;
+
+  let res;
+  killWait().then(() => (res = true));
+  await wait(100);
+  expect(res).toBe(true);
+});
+
+test(`Waits for processes to close`, async () => {
+  expect.assertions(2);
+  reset();
+  populate();
+  store.options.spawned.wait = 'bind';
+  store.options.spawned.sigterm = 5000;
+  // @ts-ignore
+  store.processes.foo.ps.on.mockImplementation(implementation);
+  store.processes.bar.running = false;
+  // @ts-ignore
+  store.processes.baz.ps.on.mockImplementation(implementation);
+
+  let res;
+  killWait().then(() => (res = true));
+
+  await wait(500);
+  expect(res).not.toBe(true);
+  await wait(350);
+  expect(res).toBe(true);
+});
+
+test(`Sends SIGTERM if not closed`, async () => {
+  expect.assertions(6);
+  reset();
+  populate();
+  store.options.spawned.wait = 'bind';
+  store.options.spawned.sigterm = 1500;
+  store.options.spawned.sigkill = 5000;
+  // @ts-ignore
+  store.processes.foo.ps.on.mockImplementation(implementation);
+  store.processes.bar.running = false;
+  let cb = () => {};
+  // @ts-ignore
+  store.processes.baz.ps.on.mockImplementation((_, _cb) => {
+    cb = _cb;
+  });
+
+  let res;
+  killWait().then(() => (res = true));
+
+  await wait(1000);
+  expect(res).not.toBe(true);
+  expect(store.processes.baz.ps.kill).not.toHaveBeenCalled();
+
+  await wait(750);
+  expect(res).not.toBe(true);
+  expect(store.processes.foo.ps.kill).not.toHaveBeenCalled();
+  expect(store.processes.baz.ps.kill).toHaveBeenCalledWith('SIGTERM');
+
+  cb();
+  await wait(100);
+  expect(res).toBe(true);
+});
+
+test(`Sends SIGKILL if not closed`, async () => {
+  expect.assertions(6);
+  reset();
+  populate();
+  store.options.spawned.wait = 'bind';
+  store.options.spawned.sigterm = null;
+  store.options.spawned.sigkill = 1500;
+  // @ts-ignore
+  store.processes.foo.ps.on.mockImplementation(implementation);
+  store.processes.bar.running = false;
+  let cb = () => {};
+  // @ts-ignore
+  store.processes.baz.ps.on.mockImplementation((_, _cb) => {
+    cb = _cb;
+  });
+
+  let res;
+  killWait().then(() => (res = true));
+
+  await wait(1000);
+  expect(res).not.toBe(true);
+  expect(store.processes.baz.ps.kill).not.toHaveBeenCalled();
+
+  await wait(750);
+  expect(res).not.toBe(true);
+  expect(store.processes.foo.ps.kill).not.toHaveBeenCalled();
+  expect(store.processes.baz.ps.kill).toHaveBeenCalledWith('SIGKILL');
+
+  cb();
+  await wait(100);
+  expect(res).toBe(true);
+});
+
+test(`Doesn't send signals if null`, async () => {
+  expect.assertions(3);
+  reset();
+  populate();
+  store.options.spawned.wait = 'bind';
+  store.options.spawned.sigterm = null;
+  store.options.spawned.sigkill = null;
+  // @ts-ignore
+  store.processes.foo.ps.on.mockImplementation(implementation);
+  store.processes.bar.running = false;
+  // @ts-ignore
+  store.processes.baz.ps.on.mockImplementation(implementation);
+
+  let res;
+  killWait().then(() => (res = true));
+
+  await wait(1000);
+  expect(res).toBe(true);
+  expect(store.processes.foo.ps.kill).not.toHaveBeenCalled();
+  expect(store.processes.baz.ps.kill).not.toHaveBeenCalled();
+});
